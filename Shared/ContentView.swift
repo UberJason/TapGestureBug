07abe0f7e4c6131cf60c7faedb5e89632df569ea
Jason Ji <uberjason@gmail.com>
2020-11-30 20:21:33 -0500

    Initial commit - project that works on iOS and Mac native app (AppKit), but not in Mac Catalyst app

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var greenOn = false
+    @State private var blueOn = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            Rectangle()
+                .fill(Color.green)
+                .opacity(greenOn ? 1.0 : 0.5)
+                .frame(width: 100, height: 100)
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    greenOn.toggle()
+                    print("Tapped green")
+                }
+            Rectangle()
+                .fill(Color.blue)
+                .opacity(blueOn ? 1.0 : 0.5)
+                .frame(width: 100, height: 100)
+                .onTapGesture {
+                    blueOn.toggle()
+                    print("Tapped blue")
+                }
+        }
     }
 }
 
