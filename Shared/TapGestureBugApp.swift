e72b5c86fa4d33590dea94ec01e40e74273efcc5
Jason Ji <uberjason@gmail.com>
2020-11-30 20:17:06 -0500

    Initial Commit

---
+++ Shared/TapGestureBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapGestureBugApp.swift
+//  Shared
+//
+//  Created by Jason Ji on 11/30/20.
+//
+
+import SwiftUI
+
+@main
+struct TapGestureBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
