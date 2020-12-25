8e586be3cb6fe87322b02723d1fd61f34cf42564
Jason Ji <uberjason@gmail.com>
2020-12-25 15:55:40 -0500

    Added UIViewRepresentable workaround and demonstrate it also does not work

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # TapGestureBug
 
-This project shows a bug with SwiftUI onTapGesture in Mac Catalyst apps on Big Sur. onTapGesture works fine on iOS and Mac native (AppKit) apps.
+This project shows a bug with SwiftUI onTapGesture in Mac Catalyst apps on Big Sur. onTapGesture works fine on iOS and Mac native (AppKit) apps. In this project, the green and blue squares use onTapGesture, while the orange square (only available in iOS and Catalyst) uses a UIViewRepresentable wrapper with a UITapGestureRecognizer.
 
 Steps to reproduce:
 1. Run the project using the TapGestureBug (iOS) scheme in an iPhone simulator, and observe that tapping the green and blue squares always toggles their state (from 5% to 100% opacity), also printing "green tapped" or "blue tapped" in the console.
 2. Run the project using the TapGestureBug (macOS) scheme for Mac, observing the same correct behavior.
 3. Run the project using the TapGestureBug (iOS) scheme on My Mac, which is the Catalyst build. The behavior is *not* correct:
     * Observe that tapping green or blue squares *often does not toggle their state.*
+    * The behavior may start out consistent, but once you start tapping different squares, it all falls apart.
     * Also observe that the console often prints a statement similar to "Invalid timestamps for HID response delay: 8104836708916 to 194516280245"
--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,10 +6,15 @@
 //
 
 import SwiftUI
+#if canImport(UIKit)
+import UIKit
+#endif
 
 struct ContentView: View {
     @State private var greenOn = false
     @State private var blueOn = false
+    @State private var orangeOn = false
+    
     var body: some View {
         HStack {
             Rectangle()
@@ -29,9 +34,62 @@ struct ContentView: View {
                     blueOn.toggle()
                     print("Tapped blue")
                 }
+            #if canImport(UIKit)
+            Rectangle()
+                .fill(Color.orange)
+                .opacity(orangeOn ? 1.0 : 0.5)
+                .frame(width: 100, height: 100)
+                .overlay(
+                    TappableView(onTap: {
+                        orangeOn.toggle()
+                        print("Tapped orange")
+                    })
+                )
+            #endif
         }
     }
 }
+
+#if canImport(UIKit)
+struct TappableView: UIViewRepresentable {
+    let onTap: () -> ()
+    
+    func makeUIView(context: Context) -> TapView {
+        print("makeUIView - TapView")
+        return TapView {
+            print("I was tapped from a UIKit workaround!")
+            onTap()
+        }
+    }
+    
+    func updateUIView(_ uiView: TapView, context: Context) {
+        print("updateUIView")
+    }
+}
+
+class TapView: UIView {
+    private let onTap: () -> ()
+    
+    init(onTap: @escaping () -> ()) {
+        print("init TapView")
+        self.onTap = onTap
+        super.init(frame: .zero)
+        
+        addGestureRecognizer(
+            UITapGestureRecognizer(target: self, action: #selector(tapped))
+        )
+    }
+    
+    @objc func tapped() {
+        print("tapped() from UITapGestureRecognizer")
+        onTap()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) not implemented")
+    }
+}
+#endif
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
